b48b5349d50baad3a5acac8129475ea8313f19d3
Monsieur-Bandana <nicolas.wild@googlemail.com>
2025-10-26 14:20:13 +0100

    allowed google server

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    allowedHosts: ["m-hochzeit-demo-840610411426.asia-southeast2.run.app"],
+    host: true, // allow external connections
+    port: 5173, // optional, default
+  },
+});
