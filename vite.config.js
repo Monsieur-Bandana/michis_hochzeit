dde4189d2227fb7cc8d5287ec54fb959ae589871
Monsieur-Bandana <nicolas.wild@googlemail.com>
2025-11-03 03:17:08 +0100

    added michis main server

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    allowedHosts: ["m-hochzeit-demo-840610411426.asia-southeast2.run.app"],
+    allowedHosts: [
+      "m-hochzeit-demo-840610411426.asia-southeast2.run.app",
+      "michis-hochzeit-840610411426.asia-southeast2.run.app",
+    ],
     host: true, // allow external connections
     port: 5173, // optional, default
   },
